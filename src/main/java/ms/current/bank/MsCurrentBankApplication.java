4bd3f6710f19d70d5cafc0248300d9fe4a9d3e91
@EduCCor <edd.ckalb@gmail.com>
2021-11-15 19:20:01 -0500

    current account

---
+++ src/main/java/ms/current/bank/MsCurrentBankApplication.java
@@ -0,0 +1,13 @@
+package ms.current.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrentBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrentBankApplication.class, args);
+	}
+
+}
